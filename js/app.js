377c51fcecf62a06395849c1d74d36685c4590eb
waleedasad <waleedasad27@gmail.com>
2023-08-19 13:02:15 +0500

    data type js

---
+++ js/app.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+
+
+let sym1 = Symbol("JavaScript is fun!");
+let sym2 = Symbol("JavaScript is fun!");
+console.log("Value =>",sym1)
+console.log("These two Symbols are the same:", sym1 === sym2);
